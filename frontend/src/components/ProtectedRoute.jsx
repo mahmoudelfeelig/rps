35a311ad32f4ee67e8ac19922cbc8a36e0d1b72f
mahmoudelfeelig <mahmoudelfeelig@gmail.com>
2025-05-06 15:21:09 +0200

    changed the routing if you're not logged in and after you log out to the home page

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -7,7 +7,7 @@ const ProtectedRoute = ({ children }) => {
   const location = useLocation();
 
   if (!user) {
-    return <Navigate to="/NotFound" state={{ from: location }} replace />;
+    return <Navigate to="/" state={{ from: location }} replace />;
   }
 
   return children;
